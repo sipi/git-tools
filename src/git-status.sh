2f5caaf126bbf71f74721e9b9337d61075f7e6ff
Clément SIPIETER <clement@6pi.fr>
2018-11-17 16:03:52 +0100

    improve it

--- src/git-status.sh
+++ src/git-status.sh
@@ -1,13 +1,43 @@
 #!/bin/bash
 
-for file in $(git status | grep modified | cut -d ':' -f 2);
+while true;
 do
   clear;
-  git diff $file;
-  echo [(a)dd/(c)heckout/(n)ext]?;
+  git status
+  echo '[(any)ok/(s)show/(e|q)exit]'
   read n;
   case $n in
-    a) git add $file;;
-    c) git checkout $file;;
+    [eqEQ]*) exit;;
+    [sS]*) git diff --cached;;
+    *) break;;
   esac;
 done
+
+for file in $(git status -s | grep -E "^.\S" | sed 's/^...\(.*\)$/\1/');
+do
+  clear;
+  echo $file;
+  git diff $file;
+  echo '[(a|y)add/(p)patch/(c)checkout/(i|n)ignore/(e|q)exit]?';
+  read n;
+  case $n in
+    [aAyY]*) git add $file;;
+    [pP]*) git add -p $file;;
+    [cC]*) git checkout $file;;
+    [iI|nN]*) ;; # next
+    [eEqQ]*) exit;;
+    *) ;; # next
+  esac;
+done
+
+clear;
+git status;
+echo "commit ? [y/n]"
+read n;
+case $n in
+  [yY]*) 
+    echo -n "commit message: "
+    read msg;
+    git commit -m "$msg"
+    ;;
+esac
